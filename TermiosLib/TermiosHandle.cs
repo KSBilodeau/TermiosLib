7b285c5cf53a7e90956c6af4c515781b51fa7397
kangarooCoder <ksbilodeau07@gmail.com>
2021-11-03 19:11:53 -0500

    Remove unnecessary using statement in TermiosHandle.cs.

--- TermiosLib/TermiosHandle.cs
+++ TermiosLib/TermiosHandle.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Text;
 using TermiosLib.TermiosEnums;
 
 // TODO: Complete documentation in before next commit
